246ddcf13c00aaa2c234faf21a9a32cbb67e7673
Thomas Williams <thomas7.williams@hotmail.com>
2018-10-11 22:35:18 +0100

    Added intellisense comments, and incremented the version.

--- src/Extensions.DependencyInjection.Services/ServiceAttribute.cs
+++ src/Extensions.DependencyInjection.Services/ServiceAttribute.cs
@@ -2,9 +2,17 @@
 
 namespace Extensions.DependencyInjection.Services
 {
+    /// <summary>
+    /// Specifies the class being attributed should be registered for dependency injection.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public class ServiceAttribute : Attribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SerializableAttribute"/> class with the base type this service class is implementing.
+        /// </summary>
+        /// <param name="type">The base type this service implements.</param>
+        /// <param name="scope">The lifetime of the service upon initialization.</param>
         public ServiceAttribute(Type type, ServiceScope scope = ServiceScope.Scoped)
         {
             Type = type ?? throw new ArgumentNullException(nameof(type));
@@ -13,8 +21,10 @@ namespace Extensions.DependencyInjection.Services
 
         // ----- Properties ----- //
 
+        /// <summary>Gets the base type the attributed service is implementing.</summary>
         public Type Type { get; }
 
+        /// <summary>Gets the lifetime scope that will control the intialization of the attributed class.</summary>
         public ServiceScope Scope { get; }
     }
 }
--- src/Extensions.DependencyInjection.Services/ServiceDeclaration.cs
+++ src/Extensions.DependencyInjection.Services/ServiceDeclaration.cs
@@ -2,8 +2,17 @@
 
 namespace Extensions.DependencyInjection.Services
 {
+    /// <summary>
+    /// Defines the specification for a class to be registered as a service for dependency injection.
+    /// </summary>
     public class ServiceDeclaration
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServiceDeclaration"/> class with a configuration for intializing a dependency injector.
+        /// </summary>
+        /// <param name="serviceType">The base type that will be injected.</param>
+        /// <param name="declaringType">The actual implementation type of the <paramref name="serviceType"/> that will be injected.</param>
+        /// <param name="scope">The lifetime of the service upon initialization.</param>
         public ServiceDeclaration(Type serviceType, Type declaringType, ServiceScope scope)
         {
             DeclaringType = declaringType ?? throw new ArgumentNullException(nameof(declaringType));
@@ -18,10 +27,15 @@ namespace Extensions.DependencyInjection.Services
                 throw new InvalidOperationException($"A service declaration can not be made for an abstract type '{declaringType.Name}'.");
         }
 
+        // ----- Properties ----- //
+
+        /// <summary>Gets the scope that will control the lifetime of the instatiated class.</summary>
         public ServiceScope Scope { get; }
 
+        /// <summary>Gets the type of the service to be registered.</summary>
         public Type ServiceType { get; }
 
+        /// <summary>Gets the implementation type of the service to be injected.</summary>
         public Type DeclaringType { get; }
     }
 }
