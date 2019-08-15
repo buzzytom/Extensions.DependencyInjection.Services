c97e26b80639ab1869c89dcac0e0d523c35e0686
Thomas <thomas7.williams@hotmail.com>
2019-08-15 20:58:12 +0100

    Restructured the RegisterServices tests so that they read like a sentence in their folder structure and thus easier to see all the different test cases.

---
+++ src/Extensions.DependencyInjection.Services.Tests/ServiceExtensions/RegisterServices/BadRequests/GivenRegisterServicesIsInvokedWithoutAnyAssemblies.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace Extensions.DependencyInjection.Services.Tests
+{
+    [TestFixture]
+    public class GivenRegisterServicesIsInvokedWithoutAnyAssemblies
+    {
+        [Test]
+        public void ThenAnArgumentNullExceptionIsThrown()
+        {
+            ArgumentNullException exception = Assert.Throws<ArgumentNullException>(() => ServiceExtensions.RegisterServices(Mock.Of<IServiceCollection>(), null));
+            Assert.AreEqual("assemblies", exception.ParamName);
+        }
+    }
+}
---
+++ src/Extensions.DependencyInjection.Services.Tests/ServiceExtensions/RegisterServices/GivenRegisterServicesIsInvokedWithEmptyAssemblies.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
+using System.Reflection;
+
+namespace Extensions.DependencyInjection.Services.Tests
+{
+    [TestFixture]
+    public class GivenRegisterServicesIsInvokedWithEmptyAssemblies
+    {
+        private Mock<IServiceCollection> subject = null;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            subject = new Mock<IServiceCollection>();
+
+            ServiceExtensions.RegisterServices(subject.Object, new Assembly[0]);
+        }
+
+        [Test]
+        public void ThenNoServicesAreRegisteredToTheServiceCollection()
+        {
+            subject.Verify(instance => instance.Add(It.IsAny<ServiceDescriptor>()), Times.Never);
+        }
+    }
+}
