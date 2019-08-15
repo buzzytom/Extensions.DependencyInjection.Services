83dc5cf66bcdf43343b472b2482bb4c2c20f525b
Thomas <thomas7.williams@hotmail.com>
2019-08-15 21:11:05 +0100

    Restructured the AddService tests so that they read like a sentence in their folder structure and thus easier to see all the different test cases.

---
+++ src/Extensions.DependencyInjection.Services.Tests/ServiceExtensions/AddService/BadRequests/GivenAddServiceIsInvokedWithoutADeclaration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace Extensions.DependencyInjection.Services.Tests
+{
+    [TestFixture]
+    public class GivenAddServiceIsInvokedWithoutADeclaration
+    {
+        [Test]
+        public void ThenAnArgumentNullExceptionIsThrown()
+        {
+            ArgumentNullException exception = Assert.Throws<ArgumentNullException>(() => ServiceExtensions.AddService(Mock.Of<IServiceCollection>(), null));
+            Assert.AreEqual("declaration", exception.ParamName);
+        }
+    }
+}
---
+++ src/Extensions.DependencyInjection.Services.Tests/ServiceExtensions/AddService/GivenAddServiceIsInvoked.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
+
+namespace Extensions.DependencyInjection.Services.Tests
+{
+    [TestFixture(ServiceScope.Transient, ServiceLifetime.Transient)]
+    [TestFixture(ServiceScope.Scoped, ServiceLifetime.Scoped)]
+    [TestFixture(ServiceScope.Singleton, ServiceLifetime.Singleton)]
+    public sealed class GivenAddServiceIsInvoked
+    {
+        private readonly ServiceScope declartionScope;
+        private readonly ServiceLifetime expectedLifetime;
+        Mock<IServiceCollection> services = null;
+
+        public GivenAddServiceIsInvoked(ServiceScope declartionScope, ServiceLifetime expectedLifetime)
+        {
+            this.declartionScope = declartionScope;
+            this.expectedLifetime = expectedLifetime;
+        }
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            services = new Mock<IServiceCollection>();
+            ServiceDeclaration declaration = new ServiceDeclaration(typeof(ITestInterface), typeof(TestImplementation), declartionScope);
+
+            ServiceExtensions.AddService(services.Object, declaration);
+        }
+
+        [Test]
+        public void ThenAServiceIsAddedWithTheDescriptorMappedVerbatim()
+        {
+            services.Verify(instance => instance.Add(It.Is<ServiceDescriptor>(x =>
+                x.Lifetime == expectedLifetime &&
+                x.ServiceType == typeof(ITestInterface) &&
+                x.ImplementationType == typeof(TestImplementation))), Times.Once);
+        }
+    }
+}
