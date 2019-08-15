4ea0804d01e41d6be134fc197d4c4f28c206df9f
Thomas <thomas7.williams@hotmail.com>
2019-08-15 21:19:35 +0100

    Restructured the GetServiceDeclaration tests so that they read like a sentence in their folder structure and thus easier to see all the different test cases.

---
+++ src/Extensions.DependencyInjection.Services.Tests/ServiceExtensions/GetServiceDeclarations/BadRequests/GivenGetServiceDeclarationsIsInvokedWithoutAssemblies.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Reflection;
+
+namespace Extensions.DependencyInjection.Services.Tests
+{
+    [TestFixture]
+    public class GivenGetServiceDeclarationsIsInvokedWithoutAssemblies
+    {
+        [Test]
+        public void ThenAnArgumentNullExceptionIsThrown()
+        {
+            Assembly[] assemblies = null;
+            ArgumentNullException exception = Assert.Throws<ArgumentNullException>(() => ServiceExtensions.GetServiceDeclarations(assemblies));
+            Assert.AreEqual("assemblies", exception.ParamName);
+        }
+    }
+}
---
+++ src/Extensions.DependencyInjection.Services.Tests/ServiceExtensions/GetServiceDeclarations/GivenGetServiceDeclarationsIsInvokedWithAnAssembly.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using System.Reflection;
+
+namespace Extensions.DependencyInjection.Services.Tests
+{
+    [TestFixture]
+    public class GivenGetServiceDeclarationsIsInvokedWithAnAssembly
+    {
+        private ServiceDeclaration result = null;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            Assembly assembly = typeof(TestImplementation).Assembly;
+
+            result = ServiceExtensions
+                .GetServiceDeclarations(assembly)
+                .Single();
+        }
+
+        [Test]
+        public void ThenTheServiceDescriptorIsReturned()
+        {
+            Assert.AreEqual(typeof(TestImplementation), result.DeclaringType);
+            Assert.AreEqual(typeof(ITestInterface), result.ServiceType);
+            Assert.AreEqual(ServiceScope.Transient, result.Scope);
+        }
+    }
+}
